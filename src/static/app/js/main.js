749b908b49ef7d07f5a9eb8bb16b40590d909beb
Sarah Harissis <sarahharissis@gmail.com>
2018-04-18 12:34:04 -0400

    Updated images, updated JS to pass linting, Sass cleanup, added social icons to first panel

--- src/static/app/js/main.js
+++ src/static/app/js/main.js
@@ -1,13 +1,49 @@
-$(function () {
+/*global window:false */
+/*global $:false */
 
+var sh = sh || {};
+
+(function ($, Modernizr, window, document, bowser) {
+
+	'use strict';
+
+	// Variables
 	var loadDelay = 1000;
 	var $panels = $('#panels');
 	var $currentYear = $('#js-current-year');
 
-	setTimeout(function () {
-		$panels.addClass('panels--ready');
-	}, loadDelay);
+	sh.main = {
 
-	$currentYear.text((new Date).getFullYear());
+		init: function () {
+
+			// Initialize panels
+			setTimeout(function () {
+				$panels.addClass('panels--ready');
+			}, loadDelay);
+
+			// Add year to footer copyright
+			$currentYear.text((new Date()).getFullYear());
+
+			this.bindUIEvents();
+
+		},
+
+		bindUIEvents: function () {
+
+			$(window).on('load scroll resize orientationchange', function (event) {
+
+			});
+
+		}
+
+	};
+
+})(window.jQuery, window.Modernizr, window, window.document, window.bowser);
+
+$(function () {
+
+	'use strict';
+
+	sh.main.init();
 
 });
